820d5ae121edf4c25ca16a728dd8defe4924b2aa
Barsnes <tobiasbarsnes@gmail.com>
2018-12-06 23:13:48 -0600

    Update 2018_12_07_011741_add_slug_to_articles.php

--- database/migrations/2018_12_07_011741_add_slug_to_articles.php
+++ database/migrations/2018_12_07_011741_add_slug_to_articles.php
@@ -14,7 +14,7 @@ class AddSlugToArticles extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->string('slug')->unique()->after('body');
+            $table->string('slug')->unique();
         });
     }
 
