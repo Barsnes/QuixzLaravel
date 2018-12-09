2823ef1fa29e12d68268b3fb982f95002b4d64a9
Barsnes <tobiasbarsnes@gmail.com>
2018-12-08 21:57:42 -0600

    Admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in as {{ $role }}
+                    You are logged in as
                     @if($role == 'Admin')
                       as an admin <br>
 
