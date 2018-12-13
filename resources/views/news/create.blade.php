6ea0aa737aa0a5ef2d504e6354736f044041583a
Barsnes <tobiasbarsnes@gmail.com>
2018-12-12 23:48:50 -0600

    remove image

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -5,40 +5,6 @@
   <script>
   tinymce.init({ selector:'textarea',
   plugins:'image link autolink code advlist imagetools spellchecker media', automatic_uploads: true, menubar: false,
-  images_upload_url: 'localhost:8000/postAcceptor.php',
-  images_upload_base_path: 'public/images/',
-  images_upload_credentials: true,
-  convert_urls: false,
-  images_upload_handler: function (blobInfo, success, failure) {
-    var xhr, formData;
-
-    xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
-    xhr.open('POST', 'localhost:8000/postAcceptor.php');
-
-    xhr.onload = function() {
-      var json;
-
-      if (xhr.status != 200) {
-        failure('HTTP Error: ' + xhr.status);
-        return;
-      }
-
-      json = JSON.parse(xhr.responseText);
-
-      if (!json || typeof json.location != 'string') {
-        failure('Invalid JSON: ' + xhr.responseText);
-        return;
-      }
-
-      success(json.location);
-    };
-
-    formData = new FormData();
-    formData.append('file', blobInfo.blob(), blobInfo.filename());
-
-    xhr.send(formData);
-  }
 });
 </script>
 @endsection
