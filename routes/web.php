4553df0fe41024294ae8a856c5644d2799283f1a
Barsnes <tobiasbarsnes@gmail.com>
2019-01-16 20:49:29 -0600

    RSS FEED

--- routes/web.php
+++ routes/web.php
@@ -54,3 +54,53 @@ Route::get('/discord', function(){
   $url = 'https://discord.gg/6QxSnV5';
   return Redirect::to($url);
 });
+
+
+Route::get('feed', function(){
+
+    // create new feed
+    $feed = App::make("feed");
+
+    // multiple feeds are supported
+    // if you are using caching you should set different cache keys for your feeds
+
+    // cache the feed for 60 minutes (second parameter is optional)
+    $feed->setCache(10, 'laravelFeedKey');
+
+    // check if there is cached feed and build new only if is not
+    if (!$feed->isCached())
+    {
+       // creating rss feed with our most recent 20 posts
+       $posts = \DB::table('articles')->orderBy('created_at', 'desc')->take(20)->get();
+
+       // set your feed's title, description, link, pubdate and language
+       $feed->title = 'Quixz eSports';
+       $feed->description = 'All articles posted to Quixz eSports';
+       $feed->logo = 'https://quixz.eu/assets/image/logo/logo_500.png';
+       $feed->link = url('feed');
+       $feed->setDateFormat('datetime'); // 'datetime', 'timestamp' or 'carbon'
+       $feed->pubdate = $posts[0]->created_at;
+       $feed->lang = 'en';
+       $feed->setShortening(true); // true or false
+       $feed->setTextLimit(100); // maximum length of description text
+
+       foreach ($posts as $post)
+       {
+          $post->description = strip_tags(substr($post->body, 0, 60));
+
+           // set item's title, author, url, pubdate, description, content, enclosure (optional)*
+           $feed->add($post->title, $post->author, URL::to('https://quixz.eu/news/' . $post->slug), $post->created_at, $post->description, $post->body);
+
+       }
+
+    }
+
+    // first param is the feed format
+    // optional: second param is cache duration (value of 0 turns off caching)
+    // optional: you can set custom cache key with 3rd param as string
+    return $feed->render('atom');
+
+    // to return your feed as a string set second param to -1
+    // $xml = $feed->render('atom', -1);
+
+});
