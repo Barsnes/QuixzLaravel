160aaf5c1b47ff73b34671f74a82c360930ee0f4
Barsnes <tobiasbarsnes@gmail.com>
2019-02-11 22:33:09 -0600

    Articles

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,11 +2,24 @@
 @section('title', 'News')
 
 @section('content')
+
+@php
+  $articleCount = 0;
+@endphp
+
 <div class="article--body">
   @foreach ($articles as $article)
+    @php
+      $articleCount ++;
+    @endphp
       <a class="article--body__card" href=" {{ url('/news', $article->slug) }}">
         <img src="{{ asset('/images/' . $article->image) }}" alt="some dummy text" og:image>
+        <h5>{{ date('d M Y', strtotime($article->created_at)) }}</h5>
         <h2>{{ $article->title }}</h2>
+        @if ($articleCount == 1)
+          <p class="news__desc">{!! strip_tags(substr($article->body, 0, 150)) !!}...</p>
+          <p>Read more...</p>
+        @endif
       </a>
   @endforeach
 </div>
