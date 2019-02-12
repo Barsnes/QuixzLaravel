de46903d6f23679fb421e9b16d9f8f0ae0d64c91
Barsnes <tobiasbarsnes@gmail.com>
2019-02-11 22:11:38 -0600

    help

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css/app.css');
+   .sass('resources/sass/app.scss', 'public/css');
 
 mix.sass('resources/sass/master.scss', 'public/css/master.css');
