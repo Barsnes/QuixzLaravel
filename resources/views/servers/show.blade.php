ef5a5fbddc43f010a0c49d0e6163d383f3405ee1
Barsnes <tobiasbarsnes@gmail.com>
2019-02-23 18:41:25 -0600

    Servers

--- resources/views/servers/show.blade.php
+++ resources/views/servers/show.blade.php
@@ -11,7 +11,7 @@ Servers
 
     @foreach ($servers as $server)
         <div class="server_content">
-          <iframe style="padding-top:.5em" src="https://cache.gametracker.com/components/html0/?host={{ $server->server }}:27015&bgColor=F8B52A&fontColor=000000&titleBgColor=2B63AF&titleColor=FFFFFF&borderColor=F8B52A&linkColor=FFFFFF&borderLinkColor=222222&showMap=1&showCurrPlayers=0&showTopPlayers=0&showBlogs=0&width=240" frameborder="0" scrolling="no" width="240" height="288"></iframe>
+          <iframe src="https://cache.gametracker.com/components/html0/?host={{ $server->server }}:27015&bgColor=F8B52A&fontColor=000000&titleBgColor=2B63AF&titleColor=FFFFFF&borderColor=F8B52A&linkColor=FFFFFF&borderLinkColor=222222&showMap=1&showCurrPlayers=0&showTopPlayers=0&showBlogs=0&width=240" frameborder="0" scrolling="no" width="240" height="288"></iframe>
           <h2>IP: {{ $server->server }}</h2>
         </div>
     @endforeach
