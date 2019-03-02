0ff13b405da1a15d650716b39d85cccc65ae2b88
Barsnes <tobiasbarsnes@gmail.com>
2019-03-02 17:15:31 -0600

    Update tournaments.blade.php

--- resources/views/admin/tournaments.blade.php
+++ resources/views/admin/tournaments.blade.php
@@ -2,29 +2,88 @@
 
 @section('content')
   <div class="col-md-12">
-    <a class="btn btn-block btn-success col-md-2 offset-2" href="/admin/tournaments/create">Add torunament</a>
+    <a class="btn btn-block btn-success col-md-2 offset-2" href="/admin/tournaments/create">Add Tournament</a>
   </div>
-  <div class="col-md-12">
+  <div class="col-md-12 mt-5">
+    <h4 class="offset-2">Ongoing Tournaments</h4>
     <div class="row offset-2" style="margin-top:2rem;">
         @foreach ($tournaments as $tourn)
-          <div class="card " style="width: 20rem;">
-            <div class="card-body">
-              <h5 class="card-title">{{ $tourn->name }}</h5>
-              <h5 class="card-title">{{ date('d F Y', strtotime($tourn->date)) }}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">{{ $tourn->team->name }}</h6>
-              @php
-                $numMatches = 0;
-              @endphp
-              @foreach ($tourn->match as $match)
-                  @php
-                    $numMatches ++;
-                  @endphp
-              @endforeach
-              <h6 class="card-title">Matches: {{ $numMatches }}</h6>
-              <a href="/admin/tournaments/{{ $tourn->id }}" class="btn btn-info btn-sm">View</a>
-              <a href="/admin/tournaments/{{ $tourn->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
+          @if ($tourn->finished == '2')
+            <div class="card " style="width: 20rem;">
+              <div class="card-body">
+                <h5 class="card-title">{{ $tourn->name }}</h5>
+                <h5 class="card-title">{{ date('d F Y', strtotime($tourn->date)) }}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $tourn->team->name }}</h6>
+                @php
+                  $numMatches = 0;
+                @endphp
+                @foreach ($tourn->match as $match)
+                    @php
+                      $numMatches ++;
+                    @endphp
+                @endforeach
+                <h6 class="card-title">Matches: {{ $numMatches }}</h6>
+                <a href="/admin/tournaments/{{ $tourn->id }}" class="btn btn-info btn-sm">View</a>
+                <a href="/admin/tournaments/{{ $tourn->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
+              </div>
             </div>
-          </div>
+          @endif
+        @endforeach
+    </div>
+  </div>
+
+  <div class="col-md-12 mt-3">
+    <h4 class="offset-2">Upcoming Tournaments</h4>
+    <div class="row offset-2" style="margin-top:2rem;">
+        @foreach ($tournaments as $tourn)
+          @if ($tourn->finished == '0')
+            <div class="card " style="width: 20rem;">
+              <div class="card-body">
+                <h5 class="card-title">{{ $tourn->name }}</h5>
+                <h5 class="card-title">{{ date('d F Y', strtotime($tourn->date)) }}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $tourn->team->name }}</h6>
+                @php
+                  $numMatches = 0;
+                @endphp
+                @foreach ($tourn->match as $match)
+                    @php
+                      $numMatches ++;
+                    @endphp
+                @endforeach
+                <h6 class="card-title">Matches: {{ $numMatches }}</h6>
+                <a href="/admin/tournaments/{{ $tourn->id }}" class="btn btn-info btn-sm">View</a>
+                <a href="/admin/tournaments/{{ $tourn->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
+              </div>
+            </div>
+          @endif
+        @endforeach
+    </div>
+  </div>
+
+  <div class="col-md-12 mt-5">
+    <h4 class="offset-2">Finished Tournaments</h4>
+    <div class="row offset-2" style="margin-top:2rem;">
+        @foreach ($tournaments as $tourn)
+          @if ($tourn->finished == '1')
+            <div class="card " style="width: 20rem;">
+              <div class="card-body">
+                <h5 class="card-title">{{ $tourn->name }}</h5>
+                <h5 class="card-title">{{ date('d F Y', strtotime($tourn->date)) }}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $tourn->team->name }}</h6>
+                @php
+                  $numMatches = 0;
+                @endphp
+                @foreach ($tourn->match as $match)
+                    @php
+                      $numMatches ++;
+                    @endphp
+                @endforeach
+                <h6 class="card-title">Matches: {{ $numMatches }}</h6>
+                <a href="/admin/tournaments/{{ $tourn->id }}" class="btn btn-info btn-sm">View</a>
+                <a href="/admin/tournaments/{{ $tourn->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
+              </div>
+            </div>
+          @endif
         @endforeach
     </div>
   </div>
