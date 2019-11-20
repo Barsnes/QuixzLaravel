a7d0f23dd6e924a49ec3cf8e8ea4290552553626
Tobias Barsnes <tobiasbarsnes@gmail.com>
2019-11-20 17:34:33 +0100

    Update TeamController.php

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -39,7 +39,7 @@ class TeamController extends Controller
 
     if (Auth::user()->role == 'Admin') {
       $this->validate($request, array(
-          'name' => 'required|max:20',
+          'name' => 'required|max:30',
           'body' => 'max:10000',
           'wins' => 'integer',
           'loss' => 'integer',
