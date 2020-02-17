c295cb5cbe6641f03497e2216f7bfcb4d545e7a7
Tobias Barsnes <tobiasbarsnes@gmail.com>
2020-02-17 12:34:01 +0100

    Update downloads.blade.php

--- resources/views/downloads.blade.php
+++ resources/views/downloads.blade.php
@@ -8,7 +8,7 @@
     <h1>Downloads</h1>
     <h6>Do not use our logo together with harmful, inappropriate, or controversial content.</h6>
     <h6>Do not edit our logo, colors, or artwork.</h6>
-    <h6>Do not use our logo for commersial purposes without our consent.</h6>
+    <h6>Do not use our logo for commercial purposes without our consent.</h6>
     <h6>Do not use our logo if it is not 100% visible, or if it is scaled to be unrecognizable.</h6>
     <h6>Do not redistribute any of our artwork without our consent.</h6>
 
