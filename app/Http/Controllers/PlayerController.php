f6010cb1422e27be4fb78aab60e747681285a2b7
Barsnes <tobiasbarsnes@gmail.com>
2021-05-06 15:23:05 +0200

    fix player team id

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -125,7 +125,10 @@ class PlayerController extends Controller
           $player->active = $request->active;
           $player->nationality = strtolower($request->nationality);
           $player->role = $request->role;
-          $player->team_id = $request->team_id;
+
+          if ($request->team_id) {
+            $player->team_id = $request->team_id;
+          }
 
 
           if ($request->hasFile('image')) {
