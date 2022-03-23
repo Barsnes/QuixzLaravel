6bce2189abcba115bf3bca0c8f412f6df423f654
Barsnes <tobiasbarsnes@gmail.com>
2022-03-23 22:42:22 +0100

    a lot of changes, hopefully nothing breaks

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css');
 
-mix.sass('resources/sass/master.scss', 'public/css/master.css').version();
+mix.sass('resources/sass/master.scss', 'public/css/master.css').options({ processCssUrls: false }).version();
