520566aad14550da11c3495be49a43e73972d3d0
Shift <shift@laravelshift.com>
2022-07-08 16:04:50 +0000

    Namespace seeders

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -1,16 +1,18 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CategoryTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //
     }
 }
